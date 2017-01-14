f0e9552b629d9bfeb6ef068ab470f425a50c5ff4
Delmond <muha.delalic@gmail.com>
2017-01-14 23:53:59 +0100

    Hotfix

--- phpMyAdmin-4.6.5.2-all-languages/config.sample.inc.php
+++ phpMyAdmin-4.6.5.2-all-languages/config.sample.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+//$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
@@ -26,10 +26,10 @@ $i = 0;
  */
 $i++;
 /* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
+//$cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
-//$cfg['Servers'][$i]['port'] = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$cfg['Servers'][$i]['port'] = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
